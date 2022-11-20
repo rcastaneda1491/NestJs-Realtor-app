1a59ffa4550a4ab741221cd8a45589dc9bc1c208
Rogelio Raúl Castañeda Flores <rcastaneda@PLUSTI.COM>
2022-11-20 16:17:56 -0600

    First commit realtor app

---
+++ src/user/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('/signup')
+    signUp() {
+        return this.authService.signup();
+    }
+}
